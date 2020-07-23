a27343071aa2fca4de92926051866f72b44aeb79
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-07-23 10:19:37 +0300

    empty search is done!

--- src/Components/InterfaceApp/InterfaceApp.jsx
+++ src/Components/InterfaceApp/InterfaceApp.jsx
@@ -3,10 +3,13 @@ import NewContact from "../Contacts/NewContact/NewContactContainer";
 
 const InterfaceApp = props => {
     const [newContactMode, setNewContactMode] = useState(false)
+    const [searchMode, setSearchMode] = useState(false)
 
     return (
         <div>
             {!newContactMode && <button onClick={() => setNewContactMode(true)}>new contact</button>}
+            <button onClick={()=>setSearchMode(!searchMode)}>Search</button>
+            {searchMode && <div>search line</div>}
             {newContactMode && <NewContact/>}
             {newContactMode && <button onClick={() => setNewContactMode(false)}>Close</button>}
         </div>
